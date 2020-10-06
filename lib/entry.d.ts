a603a6075fb8f086869ececcc131469d518423fc
Kionell <sashakostyunin1@gmail.com>
2020-10-06 17:02:42 +0300

    Fixed beatmap class in entry declaration

--- lib/entry.d.ts
+++ lib/entry.d.ts
@@ -1,7 +1,7 @@
 export { default as Difficulty } from "./calculator/difficultyCalc";
 export { default as Performance } from "./calculator/performanceCalc";
 export { default as Mods } from "./classes/mods";
-export { default as Beatmap } from "./services/beatmapBuilder";
-export { default as Score } from "./classes/score.js";
+export { default as Beatmap } from "./classes/beatmap";
+export { default as Score } from "./classes/score";
 export { default as getUserScores } from "./services/scoreRequester";
 export { default as getUserData } from "./services/userRequester";
