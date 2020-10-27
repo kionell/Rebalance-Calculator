20cfebba63fed2e73a8c8724ec48a8edbd98ee3e
Kionell <sashakostyunin1@gmail.com>
2020-10-27 21:19:46 +0200

    Calculator updated to latest release from 23.10.2020

--- lib/calculator/preprocessing/osuMovement.d.ts
+++ lib/calculator/preprocessing/osuMovement.d.ts
@@ -1,16 +1,28 @@
 import {HitObject} from 'osu-bpdpc/src/Beatmap';
-import LinearSpline from '../mathUtil/numerics/linearSpline';
 
 export default class OsuMovement
 {
-  private static readonly t_ratio_threshold = 1.4;
-  private static readonly correction0_still = 0;
-
-  private static correction0_moving_spline: LinearSpline;
-
+  /**
+   * New osu movement instance.
+   * @param rawMovementTime Uncorrected movement time
+   * @param distance Corrected distance between objects
+   * @param movementTime Corrected movement time
+   * @param indexOfPerformance Movement index of performance
+   * @param cheesability Cheesablility of the movement
+   * @param cheesableRatio Cheesable time ratio
+   * @param time Object start time
+   * @param endsOnSlider Movement ends on a slider?
+   */
   constructor(
-    rawMt: number, D: number, Mt: number, Ip12: number, cheesability: number, 
-    cheesableRatio: number, time: number, endsOnSlider: boolean);
+    rawMovementTime : number, 
+    distance: number, 
+    movementTime: number, 
+    indexOfPerformance: number, 
+    cheesability: number, 
+    cheesableRatio: number, 
+    time: number, 
+    endsOnSlider: boolean
+  );
 
   /**
    * Extracts movement (only for the first object in a beatmap).
@@ -18,12 +30,22 @@ export default class OsuMovement
   static extractMovement1(obj: HitObject): OsuMovement[];
 
   /**
-   * Calculates the movement time, effective distance and other details for the movement from obj1 to obj2.
+   * Calculates the movement time, effective distance and other details for the movement from objPrev to objCurr.
+   * @param objNeg4 Object that that was three objects before current</param>
+   * @param objNeg2 Prevprev object</param>
+   * @param objPrev Previous object</param>
+   * @param objCurr Current object</param>
+   * @param objNext Next object</param>
+   * @param tapStrain Current object tap strain</param>
+   * @param noteDensity Current object visual note density</param>
+   * @param clockRate Clock rate</param>
+   * @param hidden Are we calculating hidden mod?</param>
+   * @returns List of movements related to current object</returns>
    */
   static extractMovement2(
-    obj0: HitObject, obj1: HitObject, obj2: HitObject, obj3: HitObject, 
+    objNeg2: HitObject, objPrev: HitObject, objCurr: HitObject, objNext: HitObject, 
     tapStrain: number[], clockRate: number, hidden: boolean, 
-    noteDensity: number, objMinus2?: HitObject | null): OsuMovement[];
+    noteDensity: number, objNeg4?: HitObject | null): OsuMovement[];
 
   static getEmptyMovement(time: number): OsuMovement;
 
